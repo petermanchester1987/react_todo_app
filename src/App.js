3a83ba4b8c1971ea88d193b2628842b1b36f87d4
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-07-27 19:25:54 +0100

    added about page and router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Todos from "./components/Todos.js";
 import AddTodo from "./components/AddTodo.js";
+import About from "./components/pages/About.js";
 import Header from "./components/layout/Header.js";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 class App extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Take out the Trash",
         completed: false,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Make Dinner",
         completed: true,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Hoover up house",
         completed: false,
       },
@@ -45,20 +48,41 @@ class App extends React.Component {
     });
   };
 
+  //add Todo
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
-    console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 
 export class AddTodo extends Component {
+  state = {
+    title: "",
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({ title: "" });
+  };
+
+  // THIS IS USEABLE FOR MANY DIFFERENT CHANGES IN STATE VALUE
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
-      <form style={{ display: "flex" }}>
+      <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
         <input
           type="text"
           name="title"
           placeholder="Add Todo..."
           style={{ flex: "10", padding: "5px" }}
+          value={this.state.title}
+          onChange={this.onChange}
         ></input>
         <input
           type="submit"
